467aa5159bb2db4acab7b5c970b0e5e9a42a8812
Yura Gevorgyan <yurgevorgyan02@gmail.com>
2023-09-12 21:30:05 +0400

    homework Sort

--- src/homeworks/ArrayUtilTest.java
+++ src/homeworks/ArrayUtilTest.java
@@ -3,5 +3,7 @@ package homeworks;
 public class ArrayUtilTest {
     public static void main(String[] args) {
         ArrayUtil arrayUtil = new ArrayUtil();
+        arrayUtil.numbers = new int[]{1, 2, 3, 4, 9, 80, 90, 60, 80, 12, 45};
+        arrayUtil.lastNum();
     }
 }
