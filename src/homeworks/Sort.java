2ea19a6bd54ce5f5965e5b8d142074b47012a202
Yura Gevorgyan <yurgevorgyan02@gmail.com>
2023-09-18 07:12:38 +0400

    Chapter 6 DynamicArray

--- src/homeworks/Sort.java
+++ src/homeworks/Sort.java
@@ -1,13 +1,15 @@
 package homeworks;
 
 public class Sort {
-    public static void main(String[] args) {
-        int[] array = {4, 7, 1, 3, 9, 0, 2};
+    int[] array = new int[]{};
+
+    void sortFromMin() {
+
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     int y = array[j];
-                    array[j] = array[j+1];
+                    array[j] = array[j + 1];
                     array[j + 1] = y;
                 }
             }
@@ -15,5 +17,22 @@ public class Sort {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
+        System.out.println();
+    }
+
+    void sortFromMax() {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    int y = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = y;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
     }
 }
---
+++ src/homeworks/SortTest.java
@@ -0,0 +1,10 @@
+package homeworks;
+
+public class SortTest {
+    public static void main(String[] args) {
+        Sort sort = new Sort();
+        sort.array=new int[]{21,23,4,35564,78,3,9,0,11,98};
+        sort.sortFromMax();
+        sort.sortFromMin();
+    }
+}
